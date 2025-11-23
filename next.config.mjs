58dcc9363f8c8991778f9e32b63774caab6e4580
Shreya-ray1206 <sriya.ray@kibbcom.com>
2025-11-23 06:06:36 +0530

    deployment fix 9

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,9 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {}, // empty to satisfy Turbopack
+  experimental: {
+    turbo: false, // disable Turbopack
+  },
   webpack(config) {
     config.resolve.alias["@"] = path.resolve("./"); // optional "@/*" alias
     return config;
